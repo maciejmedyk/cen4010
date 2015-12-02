1e2ef2807fed1f869981a25a006b7a1b3c4e4d98
Maciej Medyk <mmedyk@fau.edu>
2015-12-02 16:41:42 -0500

    new database bluemix test

--- connection.php
+++ connection.php
@@ -4,7 +4,8 @@
 ?>
 
 <?php
-$db = mysqli_connect('us-cdbr-iron-east-02.cleardb.net', 'b041107957eecd', '31d4176a', 'ad_5b54bf9bc4c55af')
-//$db = mysqli_connect('134.168.8.146', 'root', '31d4176a', 'ad_5b54bf9bc4c55af')
+//$db = mysqli_connect('us-cdbr-iron-east-02.cleardb.net', 'b041107957eecd', '31d4176a', 'ad_5b54bf9bc4c55af')
+//$db = mysqli_connect('localhost','mmedyk','75QNrXUROmsEX','ad_5b54bf9bc4c55af')
+$db = mysqli_connect('134.168.8.146','root','31d4176a','ad_5b54bf9bc4c55af')
 	or die(mysqli_connect_error());
 ?>
