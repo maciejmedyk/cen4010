7a2abf9456c82b5128a4d3cae6ac98c0c223f4f8
Nicholas Kehagias <nick@itsjustwhatever.com>
2015-12-04 10:59:10 -0500

    Finished driver location update

--- driver/updateLocation.php
+++ driver/updateLocation.php
@@ -1,14 +1,21 @@
 <?php
-
+include_once("session.php"); 
     if(isset($_POST['dID']) && isset($_POST['lng']) && isset($_POST['lat'])){
         $lat = $_POST['lat'];
         $lng = $_POST['lng'];
         $dID = $_POST['dID'];
+        
+        $query = "UPDATE drivers SET curLat='$lat', curLng='$lng' WHERE dID=$dID;";
+        $sql = $db->query($query);
+
+        if($sql){
+            echo "Successfully updated driver " . $dID . " with location: " . $lat . "  " . $lng;
+        }else{
+            echo "There was an SQL Error:  " . $sql->error;
+        }
     }else{
-        return;
+        echo "Driver Location Update failed.";
     }
-    echo $lat . "  " . $lng . "  " . $dID;
-    //$query = "UPDATE drivers SET curLat=$lat, curLng=$lng WHERE dID=$dID;";
-    //$sql = $db->query($query);
+
 
 ?>
